9b87dcbf59e6e714e990ef436d5d4ff74f883eab
suggsr2235 <112404820+suggsr2235@users.noreply.github.com>
2022-09-13 09:51:41 -0500

    Update and rename Code.py to Final Code.py
    No code errors that I can see

--- Final Code.py
+++ Final Code.py
@@ -21,7 +21,7 @@ class RussianRoulette:
         
         else:
             if value > 6:
-                print("too many players!")
+                raise ValueError("ERROR:TOO MANY PLAYERS")
             else:
                 return value
                 
@@ -41,17 +41,18 @@ class RussianRoulette:
             player_name = input(f"What's Player {number}'s name: ")
             self.numofplay.append(player_name)
             self.choicelist.append(player_name)
-    
+           
+        
     def start_game(self):
         print(
-            """Random Russian Roulette: Safe edition!\n
+            """Russian Roulette: Safe edition!\n
             Each turn is random instead of one after the other, so you may go twice in a row!\n
             """
         )
         
         self.set_num_players()
         self.set_player_names()
-        
+        random.shuffle(self.bullet)
     def play_game(self):
         while len(self.numofplay) > 1:
             goes1 = random.choice(self.numofplay)
@@ -63,45 +64,59 @@ class RussianRoulette:
             
           
             
-            print((go) + " goes now!")
-            self.choicelist.remove(go)
+            print((go), " goes now!")
+            #print(self.numofplay)
+            
             other = random.choice(self.choicelist)
             # print(choicelist)
+            spin = input("would you like to spin (y/n)")
+            if spin == "y":
+              print("shuffling now!")
+              random.shuffle(self.bullet)
+              #print(self.bullet)
+            elif spin == "n":
+              print("Ok cool")
+            else:
+               raise ValueError("ERROR:NOT ACCEPTABLE")
             choice1 = int(input("hit yourself(1) or a random player(2)"))
             if choice1 == 1:
-              shoot1 == self.bullet[0]
+              shoot1 = self.bullet[0]
               
-              self.numofplay.pop(0)
-              self.numofplay.append(shoot1)
+              self.bullet.pop(0)
+              self.bullet.append(shoot1)
               if shoot1 == 1:
                self.numofplay.remove(go)
                print (go + " was hit!")
+               random.shuffle(self.bullet)
                     
               else:
                 print(go + " was spared!")
+                #print(self.numofplay)
                 self.choicelist.append(go)
                     
             elif choice1 == 2:
                 print(other + " was chosen to get aimed at")
                 shoot2 = self.bullet[0]
               
-                self.numofplay.pop(0)
-                self.numofplay.append(shoot2)
+                self.bullet.pop(0)
+                self.bullet.append(shoot2)
                 
                 if shoot2 == 1:
                     self.numofplay.remove(other)
                     print(other + " was hit!")
                     self.choicelist.append(go)
+                    random.shuffle(self.bullet)
                 else:
                     print(other + " was spared! now it's " + go + "'s time to go!")
                     shoot3 = self.bullet[0]
               
-                    self.numofplay.pop(0)
-                    self.numofplay.append(shoot3)
+                    self.bullet.pop(0)
+                    self.bullet.append(shoot3)
                     
                     if shoot3 == 1:
                         self.numofplay.remove(go)
                         print(go + " was hit!")
+                        random.shuffle(self.bullet)
                         
                     else:
                         print(go + " was spared!")
